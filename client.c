01f4327de717eec5ebe8241004fda2116d3e1e8f
RoomieNaser <rummannaser@gmail.com>
2026-09-16 16:33:19 +0000

    Revert fixing

--- client.c
+++ client.c
@@ -4,127 +4,127 @@
 #include <unistd.h>
 #include <pthread.h>
 #include <arpa/inet.h>
-
 #include "file_utils.h"
 #include "cipher.h"
 
 char my_key[50] = "";
 
 void* receive_messages(void* arg){
-	int sock = *(int*)arg;
-	char buffer[MAX_FILE_SIZE + 1024];
+    int sock = *(int*)arg;
+    char buffer[MAX_FILE_SIZE + 1024];
 
-	while(1) {
-	
-		memset(buffer, 0, sizeof(buffer));
-		int readBytes = read(sock, buffer, sizeof(buffer));
+    while(1) {
+        memset(buffer, 0, sizeof(buffer));
+        int readBytes = read(sock, buffer, sizeof(buffer));
 
-		//same disconnect handling as server
-		if (readBytes <= 0){
-			printf("Shroom Disconnected :(\n");
-			exit(0);
-		}
+        if (readBytes <= 0){
+            printf("Shroom Disconnected :(\n");
+            exit(0);
+        }
 
-		if (strlen(my_key) > 0) {
-            		xor_cipher(buffer, readBytes, my_key);
-        	}
+        if (strlen(my_key) > 0) {
+            xor_cipher(buffer, readBytes, my_key);
+        }
 
-		if (strncmp(buffer, "RECVFILE FROM ", 14) == 0) {
-    			char sender[50], filename[256];
-    			long file_size;
-    			char *newline_pos = strchr(buffer, '\n');
-    			if (newline_pos) {
-        			*newline_pos = '\0';
-        			sscanf(buffer, "RECVFILE FROM %s %s %ld", sender, filename, &file_size);
-        			*newline_pos = '\n';
-        			char *payload = newline_pos + 1;
-        			save_received_file(filename, payload, file_size);
-        			printf("\nRECVFILE FROM %s: %s (%ld bytes)\n[content saved to ./received_%s]\n> ", sender, filename, file_size, filename);
-        			fflush(stdout);
-        			continue;
-    			}
-		}
-		printf("\n%s\n> ", buffer);
-		fflush(stdout);
-	}
-	return NULL;
+        if (strncmp(buffer, "RECVFILE FROM ", 14) == 0) {
+            char sender[50], filename[256];
+            long file_size;
+            
+            char *newline_pos = strchr(buffer, '\n');
+            if (newline_pos) {
+                *newline_pos = '\0';
+                sscanf(buffer, "RECVFILE FROM %s %s %ld", sender, filename, &file_size);
+                *newline_pos = '\n';
+                
+                char *payload = newline_pos + 1;
+                save_received_file(filename, payload, file_size);
+                
+                printf("\nRECVFILE FROM %s: %s (%ld bytes)\n[content saved to ./received_%s]\n> ", 
+                       sender, filename, file_size, filename);
+                fflush(stdout);
+                continue;
+            }
+        }
+
+        printf("\n%s\n> ", buffer);
+        fflush(stdout);
+    }
+    return NULL;
 }
 
-//main stuff for client
 int main(int argc, char const *argv[]){
-	if (argc != 3){
-		printf("Usage: ./client <server_ip> <port>\n");
-		return 1;
-	}
+    if (argc != 3){
+        printf("Usage: ./client <server_ip> <port>\n");
+        return 1;
+    }
 
-	int sock = 0;
-	struct sockaddr_in serv_addr;
-	char input_buffer[2048];
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char input_buffer[MAX_FILE_SIZE + 1024];
 
-	sock = socket(AF_INET, SOCK_STREAM, 0);
-	serv_addr.sin_family = AF_INET;
-	serv_addr.sin_port = htons(atoi(argv[2]));
-	
-	inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
-	connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
-	printf("Welcome to Shroom. Ready to Register client!\n");
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(atoi(argv[2]));
+    
+    inet_pton(AF_INET, argv[1], &serv_addr.sin_addr);
+    connect(sock, (struct sockaddr*)&serv_addr, sizeof(serv_addr));
+    printf("Welcome to Shroom. Ready to Register client!\n");
 
-	//thread spawning to handle async server messages
-	pthread_t recv_thread;
-	pthread_create(&recv_thread, NULL, receive_messages, (void*)&sock);
+    pthread_t recv_thread;
+    pthread_create(&recv_thread, NULL, receive_messages, (void*)&sock);
 
-	while(1) {
-	
-		printf("> ");
-		fgets(input_buffer, sizeof(input_buffer), stdin);
+    while(1) {
+        printf("> ");
+        fgets(input_buffer, sizeof(input_buffer), stdin);
+        input_buffer[strcspn(input_buffer, "\n")] = 0;
+        
+        if (strcmp(input_buffer, "QUIT") == 0){
+            write(sock, input_buffer, strlen(input_buffer));
+            close(sock);
+            break;
+        }
 
-		input_buffer[strcspn(input_buffer, "\n")] = 0;
-		
-		//if input is QUIT the server cleans up without affecting other clients
-		if (strcmp(input_buffer, "QUIT") == 0){
-			write(sock, input_buffer, strlen(input_buffer));
-			close(sock);
-			break;
-		
-		}
+        int send_len = strlen(input_buffer);
 
-		// Save the key when registering
-		if (strncmp(input_buffer, "REGISTER ", 9) == 0) {
-    			sscanf(input_buffer, "REGISTER %*s KEY %s", my_key);
-		}
-		// Encrypt standard messages
-		if (strlen(my_key) > 0 && strncmp(input_buffer, "SENDFILE", 8) != 0) {
-    			xor_cipher(input_buffer, strlen(input_buffer), my_key);
-		}
-		if (strncmp(input_buffer, "SENDFILE TO ", 12) == 0) {
-    			char target[50], filepath[256];
-    			if (sscanf(input_buffer, "SENDFILE TO %[^:]: %s", target, filepath) == 2) {
-        			if (!is_txt_extension(filepath)) {
-            				printf("ERROR only .txt files are supported\n");
-            				continue;
-        			}
-        			char file_payload[MAX_FILE_SIZE];
-        			long file_size = read_file_content(filepath, file_payload);
-        			if (file_size < 0) {
-            				printf(file_size == -1 ? "ERROR file not found\n" : "ERROR file exceeds limit\n");
-            				continue;
-        			}
-        			char *filename = strrchr(filepath, '/');
-        			filename = (filename) ? filename + 1 : filepath;
-        			char network_frame[MAX_FILE_SIZE + 1024];
-        			int header_len = sprintf(network_frame, "SENDFILE TO %s %s %ld\n", target, filename, file_size);
-        			memcpy(network_frame + header_len, file_payload, file_size);
-				xor_cipher(network_frame, header_len + file_size, my_key);
-        			write(sock, network_frame, header_len + file_size);
-        			continue;
-    			}
-		}
-		write(sock, input_buffer, strlen(input_buffer));
-	}
+        if (strncmp(input_buffer, "REGISTER ", 9) == 0) {
+            char t_cmd[20], t_user[50], t_lbl[20];
+            sscanf(input_buffer, "%s %s %s %s", t_cmd, t_user, t_lbl, my_key);
+        }
 
+        if (strncmp(input_buffer, "SENDFILE TO ", 12) == 0) {
+            char target[50], filepath[256];
+            if (sscanf(input_buffer, "SENDFILE TO %[^:]: %s", target, filepath) == 2) {
+                if (!is_txt_extension(filepath)) {
+                    printf("ERROR only .txt files are supported\n");
+                    continue;
+                }
+                
+                char file_payload[MAX_FILE_SIZE];
+                long file_size = read_file_content(filepath, file_payload);
+                if (file_size < 0) {
+                    printf(file_size == -1 ? "ERROR file not found\n" : "ERROR file exceeds limit\n");
+                    continue;
+                }
+                
+                char *filename = strrchr(filepath, '/');
+                filename = (filename) ? filename + 1 : filepath;
+                
+                char network_frame[MAX_FILE_SIZE + 1024];
+                int header_len = sprintf(network_frame, "SENDFILE TO %s %s %ld\n", target, filename, file_size);
+                memcpy(network_frame + header_len, file_payload, file_size);
+                
+                xor_cipher(network_frame, header_len + file_size, my_key);
+                write(sock, network_frame, header_len + file_size);
+                continue;
+            }
+        }
 
-	return 0;
+        if (strlen(my_key) > 0 && strncmp(input_buffer, "SENDFILE", 8) != 0 && strncmp(input_buffer, "REGISTER", 8) != 0) {
+            xor_cipher(input_buffer, send_len, my_key);
+        }
+        
+        write(sock, input_buffer, send_len);
+    }
+
+    return 0;
 }
-
-
-
--- server.c
+++ server.c
@@ -12,9 +12,9 @@
 
 //for registration stuff
 typedef struct {
-	char username[50];
-	char key[50];
-	int socket;
+    char username[50];
+    char key[50];
+    int socket;
 } Client;
 
 Client clients[MAX_CLIENTS];
@@ -24,210 +24,208 @@ int client_count = 0;
 pthread_mutex_t clients_mutex = PTHREAD_MUTEX_INITIALIZER;
 
 void* handle_client(void* arg){
-	char current_user[50] = "";
-	int sock = *(int*)arg;
-	free(arg);
-	char buffer[MAX_FILE_SIZE + 1024];
+    char current_user[50] = "";
+    int sock = *(int*)arg;
+    free(arg);
+    char buffer[MAX_FILE_SIZE + 1024];
 
-	while (1) {
-		memset(buffer, 0, sizeof(buffer));
-		int readBytes = read(sock, buffer, sizeof(buffer));
+    while (1) {
+        memset(buffer, 0, sizeof(buffer));
+        int readBytes = read(sock, buffer, sizeof(buffer));
 
-		//cleaning up in case of abrupt disconnects
-		if (readBytes <= 0 || strncmp(buffer, "QUIT", 4) == 0) {
-			pthread_mutex_lock(&clients_mutex);
-			for (int i = 0; i < client_count; i++){
-				if (clients[i].socket == sock){
-					for (int j = i; j < client_count - 1; j++){
-						//leftShift the remaining clients to remove disconnected user
-						clients[j] = clients[j + 1];
-					}
-					client_count--;
-					break;
-				}
-			}
-			pthread_mutex_unlock(&clients_mutex);
+        //cleaning up in case of abrupt disconnects
+        if (readBytes <= 0 || strncmp(buffer, "QUIT", 4) == 0) {
+            pthread_mutex_lock(&clients_mutex);
+            for (int i = 0; i < client_count; i++){
+                if (clients[i].socket == sock){
+                    for (int j = i; j < client_count - 1; j++){
+                        //leftShift the remaining clients to remove disconnected user
+                        clients[j] = clients[j + 1];
+                    }
+                    client_count--;
+                    break;
+                }
+            }
+            pthread_mutex_unlock(&clients_mutex);
 
-			if (strncmp(buffer, "QUIT", 4) == 0){
-				char goodbye[150];
-				sprintf(goodbye, "GOODBYE %s, Shroom will be lonely without you D: \n", current_user);
-				write(sock, goodbye, strlen(goodbye));
-			}
+            if (strncmp(buffer, "QUIT", 4) == 0){
+                char goodbye[150];
+                sprintf(goodbye, "GOODBYE %s, Shroom will be lonely without you D: \n", current_user);
+                write(sock, goodbye, strlen(goodbye));
+            }
 
-			close(sock);
-			break;
-		}
+            close(sock);
+            break;
+        }
 
-		char sender_key[50] = "";
-		pthread_mutex_lock(&clients_mutex);
-		for (int i = 0; i < client_count; i++) {
-    			if (clients[i].socket == sock) strcpy(sender_key, clients[i].key);
-		}
-		pthread_mutex_unlock(&clients_mutex);
+        char sender_key[50] = "";
+        pthread_mutex_lock(&clients_mutex);
+        for (int i = 0; i < client_count; i++) {
+                if (clients[i].socket == sock) strcpy(sender_key, clients[i].key);
+        }
+        pthread_mutex_unlock(&clients_mutex);
 
-		if (strlen(sender_key) > 0) {
-    			xor_cipher(buffer, readBytes, sender_key);
-		}
+        if (strlen(sender_key) > 0) {
+                xor_cipher(buffer, readBytes, sender_key);
+        }
 
-		char cmd[20], username[50], key_label[20], key[50];
+        char cmd[20], username[50], key_label[20], key[50];
 
-		if (sscanf(buffer, "%s %s %s %s", cmd, username, key_label, key) == 4){
-			if (strcmp(cmd, "REGISTER") == 0 && strcmp(key_label, "KEY") == 0){
-				pthread_mutex_lock(&clients_mutex);
-				int username_taken = 0;
+        if (sscanf(buffer, "%s %s %s %s", cmd, username, key_label, key) == 4){
+            if (strcmp(cmd, "REGISTER") == 0 && strcmp(key_label, "KEY") == 0){
+                pthread_mutex_lock(&clients_mutex);
+                int username_taken = 0;
 
-				for (int i = 0; i < client_count; i++){
-					if (strcmp(clients[i].username, username) == 0){
-						username_taken = 1;
-						break;
-					}
-				}
+                for (int i = 0; i < client_count; i++){
+                    if (strcmp(clients[i].username, username) == 0){
+                        username_taken = 1;
+                        break;
+                    }
+                }
 
-				if (username_taken){
-					char error_msg[100];
-					sprintf(error_msg, "WOMP WOMP username %s is already taken gng\n", username);
-					write(sock, error_msg, strlen(error_msg));
-				} else if (client_count < MAX_CLIENTS){
-					strcpy(clients[client_count].username, username);
-					strcpy(clients[client_count].key, key);
-					clients[client_count].socket = sock;
-					client_count++;
+                if (username_taken){
+                    char error_msg[100];
+                    sprintf(error_msg, "WOMP WOMP username %s is already taken gng\n", username);
+                    write(sock, error_msg, strlen(error_msg));
+                } else if (client_count < MAX_CLIENTS){
+                    strcpy(clients[client_count].username, username);
+                    strcpy(clients[client_count].key, key);
+                    clients[client_count].socket = sock;
+                    client_count++;
 
-					char success_msg[100];
-					sprintf(success_msg, "Registration Successful %s, YIPPEE\n", username);
-					write(sock, success_msg, strlen(success_msg));
-					printf("Welcome to Shroom, %s!\n", username);
-					strcpy(current_user, username);
-				}
-				pthread_mutex_unlock(&clients_mutex);
-				continue;
-			}
-		}
+                    char success_msg[150];
+                    sprintf(success_msg, "Registration Successful %s, YIPPEE\n", username);
+                    
+                    xor_cipher(success_msg, strlen(success_msg), key);
+                    
+                    write(sock, success_msg, strlen(success_msg));
+                    printf("Welcome to Shroom, %s!\n", username);
+                    strcpy(current_user, username);
+                }
+                pthread_mutex_unlock(&clients_mutex);
+                continue;
+            }
+        }
 
-		//sendTo routing setup
-		char target[50];
-		char msg[1024];
+        //sendTo routing setup
+        char target[50];
+        char msg[1024];
 
-		if (sscanf(buffer, "SEND TO %49[^:]:%1023[^\n]", target, msg) == 2){
-			char* actual_msg = msg;
-			//skipping whitespaces in the beginning
-			if (actual_msg[0] == ' ') actual_msg++;
+        if (sscanf(buffer, "SEND TO %49[^:]:%1023[^\n]", target, msg) == 2){
+            char* actual_msg = msg;
+            //skipping whitespaces in the beginning
+            if (actual_msg[0] == ' ') actual_msg++;
 
-			pthread_mutex_lock(&clients_mutex);
-			int found = 0;
+            pthread_mutex_lock(&clients_mutex);
+            int found = 0;
 
-			for (int i = 0; i < client_count; i++){
-				if (strcmp(clients[i].username, target) == 0){
-					char formatted_msg[1200];
-					sprintf(formatted_msg, "FROM %s: %s\n", current_user, actual_msg);
-					xor_cipher(formatted_msg, strlen(formatted_msg), clients[i].key);
-					write(clients[i].socket, formatted_msg, strlen(formatted_msg));
-					found = 1;
-					break;
-				}
-			}
+            for (int i = 0; i < client_count; i++){
+                if (strcmp(clients[i].username, target) == 0){
+                    char formatted_msg[1200];
+                    sprintf(formatted_msg, "FROM %s: %s\n", current_user, actual_msg);
+                    xor_cipher(formatted_msg, strlen(formatted_msg), clients[i].key);
+                    write(clients[i].socket, formatted_msg, strlen(formatted_msg));
+                    found = 1;
+                    break;
+                }
+            }
 
-			if (!found){
-				char error_msg[100];
-				sprintf(error_msg, "ERROR: %s is not online\n", target);
-				write(sock, error_msg, strlen(error_msg));
-			}
+            if (!found){
+                char error_msg[100];
+                sprintf(error_msg, "ERROR: %s is not online\n", target);
+                write(sock, error_msg, strlen(error_msg));
+            }
 
-			pthread_mutex_unlock(&clients_mutex);
-			continue;
-		}
-		
-		//File routing
-		if (strncmp(buffer, "SENDFILE TO ", 12) == 0) {
-    			char target[50], filename[256];
-    			long file_size;
+            pthread_mutex_unlock(&clients_mutex);
+            continue;
+        }
+        
+        //File routing
+        if (strncmp(buffer, "SENDFILE TO ", 12) == 0) {
+                char target[50], filename[256];
+                long file_size;
 
-    			// Locate the newline separating the header from the file payload
-    			char *newline_pos = strchr(buffer, '\n');
-    			if (newline_pos) {
-        		*newline_pos = '\0';
-        		sscanf(buffer, "SENDFILE TO %s %s %ld", target, filename, &file_size);
-        		*newline_pos = '\n';
+                // Locate the newline separating the header from the file payload
+                char *newline_pos = strchr(buffer, '\n');
+                if (newline_pos) {
+                *newline_pos = '\0';
+                sscanf(buffer, "SENDFILE TO %s %s %ld", target, filename, &file_size);
+                *newline_pos = '\n';
 
-        		int header_len = (newline_pos - buffer) + 1;
-        		char *payload = buffer + header_len;
+                int header_len = (newline_pos - buffer) + 1;
+                char *payload = buffer + header_len;
 
-        		pthread_mutex_lock(&clients_mutex);
-        		int found = 0;
-        		for (int i = 0; i < client_count; i++) {
-            			if (strcmp(clients[i].username, target) == 0) {
-                			char network_frame[MAX_FILE_SIZE + 1024];
-                			// Mirror the hop-by-hop routing by re-packaging as RECVFILE
-                			int out_header = sprintf(network_frame, "RECVFILE FROM %s %s %ld\n", current_user, filename, file_size);
-                			memcpy(network_frame + out_header, payload, file_size);
-					xor_cipher(network_frame, out_header + file_size, clients[i].key);
+                pthread_mutex_lock(&clients_mutex);
+                int found = 0;
+                for (int i = 0; i < client_count; i++) {
+                        if (strcmp(clients[i].username, target) == 0) {
+                            char network_frame[MAX_FILE_SIZE + 1024];
+                            // Mirror the hop-by-hop routing by re-packaging as RECVFILE
+                            int out_header = sprintf(network_frame, "RECVFILE FROM %s %s %ld\n", current_user, filename, file_size);
+                            memcpy(network_frame + out_header, payload, file_size);
+                            xor_cipher(network_frame, out_header + file_size, clients[i].key);
 
-                			write(clients[i].socket, network_frame, out_header + file_size);
-                			found = 1;
-                			break;
-            			}
-        		}
+                            write(clients[i].socket, network_frame, out_header + file_size);
+                            found = 1;
+                            break;
+                        }
+                }
 
-        		if (!found) {
-            			char error_msg[100];
-            			sprintf(error_msg, "ERROR %s is not online\n", target);
-            			write(sock, error_msg, strlen(error_msg));
-        		}
-        		pthread_mutex_unlock(&clients_mutex);
-        		continue;
-    			}
-		}
-		//garbage fallback
-		if (strncmp(buffer, "SEND ", 5) == 0 || strncmp(buffer, "SENDFILE ", 9) == 0 || strncmp(buffer, "REGISTER", 9) == 0){
-			char err_format[] = "ERROR: Invalid command format\n";
-			write(sock, err_format, strlen(err_format));
-		} else {
-			char err_unknown[] = "ERROR unknown command\n";
-			write(sock, err_unknown, strlen(err_unknown));
-		}
-
-		//decryption, re encrypts
-
-		printf("Received bytes: %d\n", readBytes);
-	}
-	pthread_exit(NULL);
+                if (!found) {
+                        char error_msg[100];
+                        sprintf(error_msg, "ERROR %s is not online\n", target);
+                        write(sock, error_msg, strlen(error_msg));
+                }
+                pthread_mutex_unlock(&clients_mutex);
+                continue;
+                }
+        }
+        //garbage fallback
+        if (strncmp(buffer, "SEND ", 5) == 0 || strncmp(buffer, "SENDFILE ", 9) == 0 || strncmp(buffer, "REGISTER", 9) == 0){
+            char err_format[] = "ERROR: Invalid command format\n";
+            write(sock, err_format, strlen(err_format));
+        } else {
+            char err_unknown[] = "ERROR unknown command\n";
+            write(sock, err_unknown, strlen(err_unknown));
+        }
+    }
+    pthread_exit(NULL);
 }	
 
 //main stuff
 int main(int argc, char const *argv[]){
-	if (argc != 2){
-		printf("Usage: ./server <port>\n");
-		return 1;
-	}
+    if (argc != 2){
+        printf("Usage: ./server <port>\n");
+        return 1;
+    }
 
-	int serverfd;
-	struct sockaddr_in address;
-	int opt = 1;
-	int port = atoi(argv[1]);
-	serverfd = socket(AF_INET, SOCK_STREAM, 0);
-	setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
+    int serverfd;
+    struct sockaddr_in address;
+    int opt = 1;
+    int port = atoi(argv[1]);
+    serverfd = socket(AF_INET, SOCK_STREAM, 0);
+    setsockopt(serverfd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt));
 
-	address.sin_family = AF_INET;
-	address.sin_addr.s_addr = INADDR_ANY;
-	address.sin_port = htons(port);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(port);
 
-	bind(serverfd, (struct sockaddr *)&address, sizeof(address));
-	listen(serverfd, SOMAXCONN);
-	printf("Shroom listening on port %d\n", port);
+    bind(serverfd, (struct sockaddr *)&address, sizeof(address));
+    listen(serverfd, SOMAXCONN);
+    printf("Shroom listening on port %d\n", port);
 
-	while(1) {
-		struct sockaddr_in client_addr;
-		socklen_t addrlen = sizeof(client_addr);
+    while(1) {
+        struct sockaddr_in client_addr;
+        socklen_t addrlen = sizeof(client_addr);
 
-		int* client_socket = malloc(sizeof(int));
-		*client_socket = accept(serverfd, (struct sockaddr *)&client_addr, &addrlen);
+        int* client_socket = malloc(sizeof(int));
+        *client_socket = accept(serverfd, (struct sockaddr *)&client_addr, &addrlen);
 
-		//spawing the actual threads
-		pthread_t thread_id;
-		pthread_create(&thread_id, NULL, handle_client, (void*)client_socket);
-		pthread_detach(thread_id);
-	}
+        //spawing the actual threads
+        pthread_t thread_id;
+        pthread_create(&thread_id, NULL, handle_client, (void*)client_socket);
+        pthread_detach(thread_id);
+    }
 
-
-	return 0;
+    return 0;
 }
